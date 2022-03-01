68d121b2b0d9c99d4867f13d28048458404c500d
akmatoff <akmatoff.exe@gmail.com>
2022-03-01 18:24:40 +0600

    added text and light chnage

--- src/Scene.js
+++ src/Scene.js
@@ -1,13 +1,17 @@
 import { Suspense } from "react"
-import { Environment, OrbitControls } from "@react-three/drei"
+import { Environment, OrbitControls, Text } from "@react-three/drei"
 import Cactus from "./components/Cactus"
 import Ground from './components/Ground'
 
-function HelloText() {
+function GreetingText() {
     return (
-        <mesh>
-            <Text>akmatoff.exe</Text>
-        </mesh>
+        <Text
+            receiveShadow
+            castShadow
+            scale={[4, 4, 4]}
+            color={"#f1f1f1"}
+            position={[0, 0.15, 0.55]}
+        >akmatoff.exe</Text>
     )
 }
 
@@ -18,8 +22,9 @@ export default function Scene() {
             <fog attach="fog" args={["#222", 0, 10]} />
             <OrbitControls />
             <Cactus />
+            <GreetingText />
             <Ground />
-            <Environment background={false} preset="dawn" />
+            <Environment background={false} preset="sunset" />
         </Suspense>
     )
 }
--- src/components/Ground.js
+++ src/components/Ground.js
@@ -2,7 +2,7 @@ import { MeshReflectorMaterial } from "@react-three/drei";
 
 export default function Ground() {
     return (
-        <mesh posiiton={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh receiveShadow posiiton={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
             <planeGeometry args={[200, 200]} />
             <MeshReflectorMaterial 
                 color={"#222"}
